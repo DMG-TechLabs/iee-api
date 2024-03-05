3f7418019a11e8f1265bb52a95a30cdf04b114bc
Konstantinos Despoinidis <kdesp2003@gmail.com>
2024-03-05 17:55:33 +0200

    added main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    const domain = "http://localhost:5173/"
+</script>
+
+<p class="usage">Usage</p>
+<div class="mockup-code bg-primary text-primary-content">
+  <pre data-prefix="$"><code>curl "{domain}lessons?semester=1"</code></pre> 
+</div>
+
+<style>
+    .usage {
+        font-size: 3em;
+        padding-bottom: 0.5em;
+    }
+
+    .mockup-code {
+        width: min-content;
+    }
+</style>
