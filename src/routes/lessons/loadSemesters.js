71865185d414a28d66264ed15a031844343dcf73
Konstantinos Despoinidis <kdesp2003@gmail.com>
2024-03-05 17:10:51 +0200

    correction

--- src/routes/lessons/loadSemesters.js
+++ src/routes/lessons/loadSemesters.js
@@ -1,13 +1,13 @@
 
-import semester1 from '../../../lib/api/1.json';
-import semester2 from '../../../lib/api/2.json';
-import semester3 from '../../../lib/api/3.json';
-import semester4 from '../../../lib/api/4.json';
-import semester5 from '../../../lib/api/5.json';
-import semester6 from '../../../lib/api/6.json';
-import semester7 from '../../../lib/api/7.json';
-import semester8 from '../../../lib/api/8.json';
-import semester9 from '../../../lib/api/9.json';
+import semester1 from '../../lib/api/1.json';
+import semester2 from '../../lib/api/2.json';
+import semester3 from '../../lib/api/3.json';
+import semester4 from '../../lib/api/4.json';
+import semester5 from '../../lib/api/5.json';
+import semester6 from '../../lib/api/6.json';
+import semester7 from '../../lib/api/7.json';
+import semester8 from '../../lib/api/8.json';
+import semester9 from '../../lib/api/9.json';
 
 export const semesters = {
     1: semester1,
